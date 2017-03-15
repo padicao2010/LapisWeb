b4612f8a0cf0e13bc2e3cc884fae7116bc68ce5a
YvanHom <yvanhom@hotmail.com>
2017-03-15 15:12:17 +0800

    Add models.

--- app.lua
+++ app.lua
@@ -1,11 +1,26 @@
 local lapis = require("lapis")
-local lfs = require("lfs")
+
+local Model = require("lapis.db.model").Model
+local MProject = Model:extend("tr_project", {
+    primary_key = "pid"
+})
+local MFile = Model:extend("tr_file", {
+    primary_key = "fid"
+})
+local MLine = Model:extend("tr_line", {
+    primary_key = { "fid", "lid" }
+})
+local MLog = Model:extend("tr_log", {
+    primary_key = { "logid" }
+})
 
 local app = lapis.Application()
 app:enable("etlua")
 app.layout = require "views.layout"
 
 app:get("/", function(self)
+    self.nproj = MProject:count()
+    self.projects = MProject:select()
     return { render = "index" }
 end)
 
