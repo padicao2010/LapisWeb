2ed8500b370eb52d2c6841b78c947e87a0bedbde
YvanHom <yvanhom@hotmail.com>
2017-03-15 15:51:12 +0800

    Add newproject post.

--- app.lua
+++ app.lua
@@ -1,4 +1,9 @@
 local lapis = require("lapis")
+local app_helpers = require("lapis.application")
+local validate = require("lapis.validate")
+
+local capture_errors, yield_error = app_helpers.capture_errors, app_helpers.yield_error
+local assert_error = app_helpers.assert_error
 
 local Model = require("lapis.db.model").Model
 local MProject = Model:extend("tr_project", {
@@ -28,8 +33,14 @@ app:get("new", "/new", function(self)
     return { render = "newproject" }
 end)
 
-app:post("new", "/new", function(self)
+app:post("new", "/new", capture_errors(function(self)
+    validate.assert_valid(self.params, {
+        { "name", exists = true, min_length = 1, max_length = 22 },
+        { "desc", exists = true, min_length = 1, max_length = 254 },
+    })
+    
+    assert(MProject:create({ pname = self.params.name, pdesc = self.params.desc }))
     return { redirect_to = self:url_for("index") }
-end)
+end))
 
 return app
