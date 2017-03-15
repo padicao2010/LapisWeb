e35530bcad358ed2d75de2570fcaf16ac91784fe
YvanHom <yvanhom@hotmail.com>
2017-03-15 13:33:39 +0800

    Add projects.

--- app.lua
+++ app.lua
@@ -1,10 +1,21 @@
 local lapis = require("lapis")
+local lfs = require("lfs")
+
+local function getProjects()
+    local t = {}
+    for d in lfs.dir("projects") do
+        table.insert(t, d)
+    end
+    return t
+end
+
 local app = lapis.Application()
 app:enable("etlua")
 app.layout = require "views.layout"
 
-app:get("/", function()
-  return { render = "index" }
+app:get("/", function(self)
+    self.projects = getProjects()
+    return { render = "index" }
 end)
 
 return app
