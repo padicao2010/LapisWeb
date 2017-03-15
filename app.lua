4fbc1d71451ce800d53206b254cf91b47bc68078
YvanHom <yvanhom@hotmail.com>
2017-03-15 12:04:05 +0800

    Add layout.

--- app.lua
+++ app.lua
@@ -1,5 +1,7 @@
 local lapis = require("lapis")
 local app = lapis.Application()
+app:enable("etlua")
+app.layout = require "views.layout"
 
 app:get("/", function()
   return "Welcome to Lapis " .. require("lapis.version")
