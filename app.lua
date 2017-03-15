d422733a03108b505c5a0396c958584e94618c20
YvanHom <yvanhom@hotmail.com>
2017-03-15 14:45:15 +0800

    Move config.lua to template.

--- app.lua
+++ app.lua
@@ -1,20 +1,11 @@
 local lapis = require("lapis")
 local lfs = require("lfs")
 
-local function getProjects()
-    local t = {}
-    for d in lfs.dir("projects") do
-        table.insert(t, d)
-    end
-    return t
-end
-
 local app = lapis.Application()
 app:enable("etlua")
 app.layout = require "views.layout"
 
 app:get("/", function(self)
-    self.projects = getProjects()
     return { render = "index" }
 end)
 
